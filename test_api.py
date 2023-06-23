53cd789610e03eda2ecafc28457018fc4da036eb
Adam Guesmia <104517014+AdamGuesmia@users.noreply.github.com>
2023-06-23 19:37:27 +0200

    Add files via upload

---
+++ test_api.py
@@ -0,0 +1,39 @@
+import requests 
+import pandas as pd 
+
+
+baseurl = 'https://rickandmortyapi.com/api'
+
+endpoint = '/character'
+
+
+def main_requests (baseurl, endpoint,x) :
+	return requests.get(baseurl + endpoint + f"?page={x}").json()
+
+
+def get_pages (response) : 
+	return response['info']['pages']
+
+
+def parse_json(response) : 
+	charlist = []
+	for item in response['results'] : 
+		char = {
+		'id' : item['id'],
+		'name' : item['name'],
+		'no_ep' : len(item['episode'])
+				}
+		charlist.append(char)
+	return charlist
+
+
+mainlist = []
+data = main_requests(baseurl,endpoint ,2)
+for x in range (1,get_pages(data) +1):
+	print (x)
+	mainlist.extend(parse_json(main_requests (baseurl, endpoint,x)))
+
+df = pd.DataFrame(mainlist)
+#print (df.head(),df.tail())
+
+df.to_csv('charlist.csv',index= False)
